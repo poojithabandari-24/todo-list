5ba7e07ce9292dae447e7793b6225259cc82f961
poojithabandari-24 <152694855+poojithabandari-24@users.noreply.github.com>
2024-02-16 23:27:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+function addtask(){
+    if(inputbox.value ===''){
+        alert("you must add your task!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value="";
+}
+ listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classlist.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+
+    }
+ }, false);
